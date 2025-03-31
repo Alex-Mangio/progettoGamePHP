2ec28212153cede873bd843107b4caa4ec8d2426
Alex-Mangio <alexmangione01@gmail.com>
2025-03-31 13:47:04 +0200

    secondo commit

---
+++ menu.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Shadow Sneak</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="container">
+    <div class="title">
+      <span class="letter">S</span>
+      <span class="letter">H</span>
+      <span class="letter">A</span>
+      <span class="letter">D</span>
+      <span class="letter">O</span>
+      <span class="letter">W</span>
+      <span class="letter"> </span>
+      <span class="letter"> </span>
+      <span class="letter"> </span>
+      <span class="letter"> </span>
+      <span class="letter">S</span>
+      <span class="letter">N</span>
+      <span class="letter">E</span>
+      <span class="letter">A</span>
+      <span class="letter">K</span>
+    </div>
+
+    <div class="menu">
+      <button class="play-button" id="openModalBtn">PLAY</button><br>
+      <button class="play-button">SKIN</button>
+    </div>
+
+    <div id="modal" class="modal">
+    <div class="modal-content">
+      <span class="close" id="closeModalBtn">&times;</span>
+      <h2>SELEZIONA IL SALVATAGGIO</h2>
+      <button class="play-button" id="openModalBtn">SALVATAGGIO 1</button><br>
+      <button class="play-button" id="openModalBtn">SALVATAGGIO 2</button><br>
+      <button class="play-button" id="openModalBtn">SALVATAGGIO 3</button><br>
+    </div>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
